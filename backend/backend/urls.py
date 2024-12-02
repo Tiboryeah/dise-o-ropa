41ce61cb667275f89f049624806ed9c04c52cfce
Tiboryeah <gerardo1991@comunidad.unam.mx>
2024-12-01 21:12:59 -0600

    Mensaje

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from .views import CuentaViewSet, DisenadorViewSet, TipoPrendaViewSet, PrendaViewSet, DisenoViewSet
 
@@ -27,4 +28,7 @@ router.register(r'disenos', DisenoViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('', include('usuarios.urls')),
 ]
+
--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Cuenta, Disenador, TipoPrenda, Prenda, Diseno
+from .serializers import CuentaSerializer, DisenadorSerializer, TipoPrendaSerializer, PrendaSerializer, DisenoSerializer
+
+class CuentaViewSet(viewsets.ModelViewSet):
+    queryset = Cuenta.objects.all()
+    serializer_class = CuentaSerializer
+
+class DisenadorViewSet(viewsets.ModelViewSet):
+    queryset = Disenador.objects.all()
+    serializer_class = DisenadorSerializer
+
+class TipoPrendaViewSet(viewsets.ModelViewSet):
+    queryset = TipoPrenda.objects.all()
+    serializer_class = TipoPrendaSerializer
+
+class PrendaViewSet(viewsets.ModelViewSet):
+    queryset = Prenda.objects.all()
+    serializer_class = PrendaSerializer
+
+class DisenoViewSet(viewsets.ModelViewSet):
+    queryset = Diseno.objects.all()
+    serializer_class = DisenoSerializer
